ac4359d643fd74c88346326847697824659b1e0d
spywareXd <rajashwin1909@gmail.com>
2024-04-24 23:42:06 +0530

    code 5

--- 3.cpp
+++ 3.cpp
@@ -1,31 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class coord
-{
+ class employee
+ {
     public:
-    float x,y;
-    coord()
+        int basic,hra,da,wages,hr,con;
+        int regular()
+        {
+            int gross;
+            gross=basic+hra+da;
+            return(gross);
+        }
+        int daily()
+        {
+            return (wages*hr);
+        }
+        int consolidate()
+        {
+            return (con);
+        }
+ };
+
+ int main()
     {
-        x=0;
-        y=0;
+        employee e;
+        cin>>e.basic>>e.hra>>e.da;
+        cin>>e.wages>>e.hr>>e.con;
+        cout<<"Regular Employee ="<<e.regular()<<endl;
+        cout<<"Daily Wages = "<<e.daily()<<endl;
+        cout<<"Consolidated"<<e.consolidate()<<endl;
     }
-    coord operator >>(coord const &obj)
-    {
-        coord res;
-        res.x=x+obj.x;
-        res.y=y+obj.y;
-    }
-    coord operator << (coord const &obj)
-    {
-        cout<<obj.x<<", "<<obj.y<<endl;
-    }
-};
-int main(){
-coord c1,c2;
-cout<<"Enter x&y of first point: "<<endl;
-cin>>c1.x>>c1.y;
-cout<<"ENter x&y of second point"<<endl;
-cin>>c2.x>>c2.y;
-cout<<"First point " <<c1.x<<" ,"<<c1.y<<endl;
-cout<<"Second point " <<c2.x<<" ,"<<c2.y<<endl;
-}
