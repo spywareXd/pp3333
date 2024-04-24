0ba4e0801eca830ec474d5846a7b1623833a4f30
spywareXd <rajashwin1909@gmail.com>
2024-04-24 23:32:45 +0530

    helo

--- 3.cpp
+++ 3.cpp
@@ -1,38 +1,31 @@
 #include<iostream>
 using namespace std;
-class base
+class coord
 {
     public:
-        int n;
-        void operator ++()
-        {
-            n++;
-            n++;
-        }
-        void operator --()
-        {
-            n--;
-            n--;
-        }
-        void disp()
-        {
-            cout<<n;
-        }
+    float x,y;
+    coord()
+    {
+        x=0;
+        y=0;
+    }
+    coord operator >>(coord const &obj)
+    {
+        coord res;
+        res.x=x+obj.x;
+        res.y=y+obj.y;
+    }
+    coord operator << (coord const &obj)
+    {
+        cout<<obj.x<<", "<<obj.y<<endl;
+    }
 };
-int main()
-{
-    base b;
-    cin>>b.n;
-    char a;
-    cin>>a;
-    if(a=='+')
-    {
-        ++b;
-    }
-    else if(a=='-')
-    {
-        --b;
-    }
-    b.disp();
-    return 0;
+int main(){
+coord c1,c2;
+cout<<"Enter x&y of first point: "<<endl;
+cin>>c1.x>>c1.y;
+cout<<"ENter x&y of second point"<<endl;
+cin>>c2.x>>c2.y;
+cout<<"First point " <<c1.x<<" ,"<<c1.y<<endl;
+cout<<"Second point " <<c2.x<<" ,"<<c2.y<<endl;
 }
